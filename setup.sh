534db8f7562dc1f4864a44631eb7d6ab5a101d45
Eric Irwin <eric.irwin@issaccorp.com>
2023-01-17 13:11:08 -0700

    Update setup.sh
    changed the model to v1

--- setup.sh
+++ setup.sh
@@ -13,17 +13,18 @@ sudo apt-get install git-lfs
 sudo -u ubuntu git lfs install --skip-smudge
 
 # download the SD model v2.1 and move it to the SD model directory
-sudo -u ubuntu git clone --depth 1 https://huggingface.co/stabilityai/stable-diffusion-2-1-base
-cd stable-diffusion-2-1-base/
-sudo -u ubuntu git lfs pull --include "v2-1_512-ema-pruned.ckpt"
+sudo -u ubuntu git clone --depth 1 https://huggingface.co/runwayml/stable-diffusion-inpainting
+cd stable-diffusion-inpainting/
+sudo -u ubuntu git lfs pull --include "sd-v1-5-inpainting.ckpt"
 sudo -u ubuntu git lfs install --force
 cd ..
-mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
-rm -rf stable-diffusion-2-1-base/
+mv stable-diffusion-inpainting/sd-v1-5-inpainting.ckpt stable-diffusion-webui/models/Stable-diffusion/
+rm -rf stable-diffusion-inpainting/
 
 # download the corresponding config file and move it also to the model directory (make sure the name matches the model name)
-wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
-cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
+wget https://github.com/runwayml/stable-diffusion/blob/main/configs/stable-diffusion/v1-inpainting-inference.yaml
+cp v1-inpainting-inference.yaml stable-diffusion-webui/models/Stable-diffusion/sd-v1-5-inpainting.yaml
+
 
 # change ownership of the web UI so that a regular user can start the server
 sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
